ceb3723858965b03597a0f86057bbf3a7ed8738e
Paul Giles <pgwar2@gmail.com>
2021-01-14 19:25:36 -0700

    update most pages for multi user add manage user route

--- mtg_app.py
+++ mtg_app.py
@@ -9,27 +9,50 @@ app = Flask(__name__)
 app.debug=True
 
 
-@app.route('/mycards')
-def mycards():
-    query = 'select * from mtg_collection where owner = "%s"' % whoami
+@app.route('/mycards<user>')
+def mycards(user):
+    query = 'select * from mtg_collection where owner = "%s"' % user
     data = db_manager.query_db(query)
-    return render_template('mycards.html',data=data)
+    return render_template('mycards.html',data=data,user=user)
 
-@app.route('/add/<id>')
+@app.route('/add/<id>', methods=['GET','POST'])
 def addcard(id):
-    data = scryfall.getcard(id)
-    db_manager.insert_card(whoami,data)
-    return redirect(url_for('mycards'))
+    user = request.form.get('user')
+    print('user selected',user
+    )
 
-@app.route('/change/<id>/<qty>')
-def removecard(id,qty):
+    data = scryfall.getcard(id)
+    db_manager.insert_card(user,data)
+    return redirect(url_for('mycards', user=user))
+
+@app.route('/change/<user>/<id>/<qty>')
+def removecard(user,id,qty):
     if int(qty) < 1:
-        query = 'delete from mtg_collection where owner = "%s" and id = "%s"' % (whoami,id)
+        query = 'delete from mtg_collection where owner = "%s" and id = "%s"' % (user,id)
     else:
-        query = 'update mtg_collection set qty = %s where owner = "%s" and id = "%s"' % (qty,whoami,id)
+        query = 'update mtg_collection set qty = %s where owner = "%s" and id = "%s"' % (qty,user,id)
 
     db_manager.query_db(query)
-    return redirect(url_for('mycards'))
+    return redirect(url_for('mycards',user=user))
+
+
+@app.route('/manageowners', methods=['GET','POST'])
+def manageowners():
+    print(request.method)
+    if request.method == 'POST':
+        data = request.form.get('ownername')
+        print('DATA',data)
+
+        db_manager.query_db('insert into owners values("%s")' % (data))
+        owners = db_manager.get_owners()
+
+        return render_template('owners.html', data=owners)
+
+    else:
+        owners = db_manager.get_owners()
+        print(owners)
+
+        return render_template('owners.html', data=owners)
 
 
 @app.route('/', methods=['GET','POST'])
@@ -45,7 +68,9 @@ def home():
         for x in data[0]:
             print(x)
 
-        return render_template('home.html', data=data)
+        owners = db_manager.get_owners()
+
+        return render_template('home.html', data=data, owners=owners)
 
     else:
 
